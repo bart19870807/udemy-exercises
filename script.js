695a67ab5e8361a66218256e8a16fb083373a7e6
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-09-26 11:59:28 +0100

    lecture 133

--- script.js
+++ script.js
@@ -49,21 +49,89 @@
 // checkIn(flight, bart);
 
 
-const oneWord = function(str){
-    return str.replace(/ /g, '').toLowerCase();
+// const oneWord = function(str){
+//     return str.replace(/ /g, '').toLowerCase();
+// }
+
+// const upperFirstWord = function(str){
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// }
+
+// const transformer = function(str, fn) {
+//     console.log(`original straing: ${str}`);
+//     console.log(`transformed string: ${fn(str)}`);
+
+//     console.log(`transformed by: ${fn.name}`);
+
+// }
+// transformer('JavaScript is the best', upperFirstWord);
+// transformer('JavaScript is the best', oneWord);
+
+// const greet = function(greeting){
+//     return function (name){
+//         console.log(`${greeting} ${name}`);
+//     };
+// };
+// const greeterHey = greet('Hey ');
+// greeterHey('Bart');
+// greeterHey('Mathias ');
+
+// greet('Hello') ('Timon');
+
+// //Challenge
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+// greetArr('HOLLA')('SIMON');
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name){
+        console.log(`${name} booked a seat on ${this.airline} 
+        flight ${this.iataCode} ${flightNum}`);
+
+        this.bookings.push({flight: `${this.iataCode} ${flightNum}`, name})
+
+    },
+
+};
+
+lufthansa.book(239, 'Teodor Smith');
+lufthansa.book(666, 'Lucian Drow');
+console.log(lufthansa);
+
+
+const euroWings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+//does NOt work
+// book(333333, 'SARAH SARAH SARAH')
+
+//CALL METHOD
+book.call(euroWings, 3333, 'SARAH SARAH');
+console.log(euroWings);
+
+book.call(lufthansa, 256, 'Marry Cooper');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss Air Line',
+    iataCode: 'SAL',
+    bookings: []
 }
 
-const upperFirstWord = function(str){
-    const [first, ...others] = str.split(' ');
-    return [first.toUpperCase(), ...others].join(' ');
-}
+book.call(swiss, 555, 'Gary Move Out')
+console.log(swiss);
 
-const transformer = function(str, fn) {
-    console.log(`original straing: ${str}`);
-    console.log(`transformed string: ${fn(str)}`);
+//APPLY method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
 
-    console.log(`transformed by: ${fn.name}`);
-
-}
-transformer('JavaScript is the best', upperFirstWord);
-transformer('JavaScript is the best', oneWord);
+book.call(swiss, ...flightData);
