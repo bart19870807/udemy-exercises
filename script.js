c4cc4e38e36349ac26b7ae6880aa13bce1918d8a
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-09-30 03:43:33 +0100

    challenge

--- script.js
+++ script.js
@@ -183,6 +183,35 @@ console.log(addVat2(23));
 
 
 
+const poll = {
+    question: 'What is your favourite programing language?',
+    options:['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+
+    registerNewAnswer(){
+        //get answer
+        const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+        console.log(answer);
+        //Register answer
+        typeof answer ==='number' && answer < this.answers.length && this.answers[answer]++;
+
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array'){
+        if(type === 'array') {
+            console.log(this.answers);
+        }else if(type === 'string'){
+            // Poll results are 13, 2, 4, 1
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+    }
+};
+poll.registerNewAnswer();
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+
+
 
 
 
